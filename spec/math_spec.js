3cd9f178e20929b7b7394cd677717a44804e7d78
smithpr@dukes.jmu.edu <paris@MacBook-Pro.local>
2016-02-24 09:56:23 -0500

    added tests for add and subtract

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,5 +1,13 @@
+// tell the test where to find the code
+var math = require('../math');
+
 describe("A simple program that does arithmetic", function() {
   it("can add two numbers", function() {
-    expect(add(3,5)).toBe(8);
+    expect(math.add(3,5)).toBe(8);
   });
+   it("can subtract two numbers", function() {
+    expect(math.sub(5,3)).toBe(2);
+  });
+
+
 });
